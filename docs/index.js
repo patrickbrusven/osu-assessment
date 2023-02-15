bc4f8329aedf8e61475ccd372bc7341cdedd849f
patrickbrusven <pjbrusven@gmail.com>
2023-02-15 12:48:54 -0800

    App object with state and functions, render template dynamically baseed on AJAX response

--- docs/index.js
+++ docs/index.js
@@ -3,14 +3,71 @@ const OSU = {
   lat: "44.5638",
   lng: "-123.2794",
 };
-
 const exerciseContainer = document.getElementById("exercise-a");
 const selectSort = document.getElementById("sort");
 const loadingState = document.getElementById("loading");
-const cityData = [];
-const sortedCityData = [];
 
 selectSort.addEventListener("change", handleSelectChange);
+
+const App = {
+  state: {
+    isLoading: true,
+    error: false,
+    errorMessage: null,
+    errorStatus: null,
+    cityData: [],
+  },
+  template() {
+    exerciseContainer.innerHTML = "";
+    let renderThis = null;
+    if (this.state.isLoading) {
+      renderThis = `<span>loading data</span>`;
+    } else if (!this.state.isLoading && this.state.error) {
+      renderThis = `<span>Error loading data</br>Error msg: ${this.state.errorMessage}</br>Error status: ${this.state.errorStatus}</span>`;
+    } else {
+      renderThis = this.renderList(this.state.cityData);
+    }
+    exerciseContainer.innerHTML = renderThis;
+  },
+  initialize() {
+    this.template();
+  },
+
+  renderList(data) {
+    exerciseContainer.innerHTML = "";
+    data.map((data, i) => {
+      return (exerciseContainer.innerHTML += `<li>City: ${data.city}<br/>Lat: ${data.lat}<br/>Lng: ${data.lng}<br/>distance to OSU: ${data.distanceToOSU}mi</li>`);
+    });
+  },
+
+  handleCityData(data, statusCode) {
+    if (statusCode !== 200) {
+      this.state.isLoading = false;
+      this.state.error = true;
+      this.state.errorMessage = data;
+      this.state.errorStatus = statusCode;
+      this.template();
+    } else {
+      const serializedData = data.map((city) => {
+        const distanceToOSU = distance(
+          OSU.lat,
+          OSU.lng,
+          city.lat,
+          city.lng,
+          "M"
+        );
+        return { ...city, distanceToOSU };
+      });
+      serializedData.forEach((city) => {
+        this.state.cityData.push(city);
+      }, this);
+      this.state.isLoading = false;
+      this.template();
+    }
+  },
+};
+
+App.initialize();
 
 function handleSelectChange(e) {
   if (selectSort.value === "asc") {
@@ -32,38 +89,21 @@ function handleSelectChange(e) {
   }
 }
 
-function reqListener() {
-  const res = JSON.parse(this.responseText);
-  const serializedData = res.map((location) => {
-    const distanceToOSU = distance(
-      OSU.lat,
-      OSU.lng,
-      location.lat,
-      location.lng,
-      "M"
-    );
-    return { ...location, distanceToOSU };
-  });
-  serializedData.forEach((city) => {
-    cityData.push(city);
-    sortedCityData.push(city);
-  });
-  loadingState.classList.add("disappear");
-  renderList(cityData);
-}
-
-function renderList(data) {
-  exerciseContainer.innerHTML = "";
-  data.map((data, i) => {
-    return (exerciseContainer.innerHTML += `<li>City: ${data.city}<br/>Lat: ${data.lat}<br/>Lng: ${data.lng}<br/>distance to OSU: ${data.distanceToOSU}mi</li>`);
-  });
-}
-
-const req = new XMLHttpRequest();
-req.addEventListener("load", reqListener);
-req.open(
+// AJAX fetch city data
+const fetchCityData = new XMLHttpRequest();
+fetchCityData.onreadystatechange = () => {
+  if (fetchCityData.readyState === 4) {
+    if (fetchCityData.status === 200) {
+      const res = JSON.parse(fetchCityData.responseText);
+      App.handleCityData(res, fetchCityData.status);
+    } else {
+      App.handleCityData(fetchCityData.statusText, fetchCityData.status);
+    }
+  }
+};
+fetchCityData.open(
   "GET",
-  "https://s3-us-west-2.amazonaws.com/cdt-web-storage/cities.json",
+  "https://s3-us-west-2.amazonaws.com/cdt-web-storage/cities.jsons",
   true
 );
-req.send();
+fetchCityData.send();
